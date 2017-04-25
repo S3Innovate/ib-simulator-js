8f14bc7303cf46d37756bb208b38b6d57fbd91fa
linanw <linanwang@msn.com>
2017-04-25 20:52:10 +0800

    Adding test code to server.js

--- server.js
+++ server.js
@@ -1,15 +1,30 @@
 var mqtt = require('mqtt')
 var client = mqtt.connect('mqtt://sjlm-mqtt.cloudapp.net')
+const readline = require('readline')
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
+
+const testTopic = 'world'
+
+rl.on('line', (input) => {
+      client.publish(testTopic, input)
+      console.log(`Published '${input} to topic '${testTopic}'`)
+})
 
 client.on('connect', function () {
-    client.subscribe('world')
-    client.publish('world', 'Hello mqtt')
+    client.subscribe(testTopic);
+    console.log(`Subcribed Topic '${testTopic}'`);
 })
 
 client.on('message', function (topic, message) {
     // message is Buffer
-    console.log(message.toString())
+    console.log(`Received '${message}' from topic '${topic}'`)
     if (message == 'stopibs') {
+        console.log('Closing...')
         client.end()
+        rl.close()
+        console.log('Bye.')
     }
 })
